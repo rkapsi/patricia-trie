a4c47ce5eb9fb3fbf1c8224bf7b7a3727a16e9bb
Roger Kapsi <rkapsi@gmail.com>
2010-11-25 10:44:49 -0500

    Updating copyright info

--- src/main/java/org/ardverk/collection/PatriciaTrieBase.java
+++ src/main/java/org/ardverk/collection/PatriciaTrieBase.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2005-2009 Roger Kapsi, Sam Berlin
+ * Copyright 2005-2010 Roger Kapsi, Sam Berlin
  *
  *   Licensed under the Apache License, Version 2.0 (the "License");
  *   you may not use this file except in compliance with the License.
--- src/main/java/org/ardverk/collection/ShortKeyAnalyzer.java
+++ src/main/java/org/ardverk/collection/ShortKeyAnalyzer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2005-2009 Roger Kapsi
+ * Copyright 2005-2010 Roger Kapsi
  *
  *   Licensed under the Apache License, Version 2.0 (the "License");
  *   you may not use this file except in compliance with the License.
